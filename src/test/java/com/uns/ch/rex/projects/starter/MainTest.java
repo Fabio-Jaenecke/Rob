5e91e95dc656c065297d748de5d53f6deba9cad6
Fabio Jaenecke <fabio.jaenecke@gmail.com>
2019-02-23 19:29:50 +0100

    changed

---
+++ src/test/java/com/uns/ch/rex/projects/starter/MainTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.uns.ch.rex.projects.starter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+/**
+ * @author fabio jaenecke
+ *
+ */
+public class MainTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
